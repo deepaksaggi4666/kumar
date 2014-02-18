6e17435d327d6f21c3b32be36d06f3bea09ab023
deepaksaggi4666 <kumar@9techies.com>
2014-02-18 11:40:32 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kumar::Application.config.secret_key_base = '134cb3bbc1bbbe4f96e45c35c82ee3873e19daf61ebd5e97e58329f96983aac208e4074dde106fb041368bc2dee10c94a0a59b8566b082a8091ab7705d86fb5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kumar::Application.config.session_store :cookie_store, key: '_kumar_session'
